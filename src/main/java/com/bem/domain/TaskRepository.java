9ec7ce50748222b71d1866852a3abaa087fa08f5
Bem-Leeeeee <2480054370@qq.com>
2017-05-07 20:50:17 +0800

    增加注解

--- src/main/java/com/bem/domain/TaskRepository.java
+++ src/main/java/com/bem/domain/TaskRepository.java
@@ -1,5 +1,11 @@
 package com.bem.domain;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Transactional
 public class TaskRepository {
 
 	public void save() {
--- src/main/java/com/bem/service/TaskService.java
+++ src/main/java/com/bem/service/TaskService.java
@@ -2,11 +2,13 @@ package com.bem.service;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bem.domain.Task;
 import com.bem.domain.TaskCreateForm;
 import com.bem.domain.TaskRepository;
 
+@Service("taskService")
 public class TaskService {
 	@Autowired
 	private TaskRepository taskRepository;
