3cb0f39d5f2a2eecd96616ec3092fed6bebb5187
Breadem <breadem@aliyun.com>
2017-06-12 10:30:00 +0800

    初步登陆

--- src/main/java/com/bem/security/CustomUserDetailsService.java
+++ src/main/java/com/bem/security/CustomUserDetailsService.java
@@ -20,8 +20,8 @@ public class CustomUserDetailsService implements UserDetailsService {
 	@Resource
 	private UserRepository userRepository;
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		LocalAuth user = userRepository.findUserByAccount(username);
+	public UserDetails loadUserByUsername(String account) throws UsernameNotFoundException {
+		LocalAuth user = userRepository.findUserByAccount(account);
 		if(user == null){
             throw new UsernameNotFoundException("not found");
         }
